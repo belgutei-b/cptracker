5a8725decc737c22707316d024f4ad7b36a9d20d
Belgutei <belgutei0323@gmail.com>
2026-01-07 21:18:08 +1100

    added proxy(middleware)

---
+++ proxy.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+import { decrypt } from "./lib/session";
+import { cookies } from "next/headers";
+
+const protectedRoutes = ["/profiles"];
+const authRoutes = ["/auth/sign-in", "/auth/sign-up"];
+
+export default async function proxy(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isAuthRoute = authRoutes.includes(path);
+
+  // Decrypt the session from the cookie
+  const cookie = (await cookies()).get("session")?.value;
+  const session = await decrypt(cookie);
+
+  // 4. Redirect to /login if the user is not authenticated
+  if (isProtectedRoute && !session?.userId) {
+    return NextResponse.redirect(new URL("/auth/sign-in", req.nextUrl));
+  }
+
+  if (isAuthRoute && session?.userId) {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+
+  return NextResponse.next();
+}
+
+// Routes Proxy should not run on
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
