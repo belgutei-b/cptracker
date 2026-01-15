93680c431020125b938eb012cf3beb84752f97bb
Belgutei <belgutei0323@gmail.com>
2026-01-16 09:57:54 +1100

    removed old version of signout

--- app/profile/action.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-import { redirect } from "next/navigation";
-import { deleteSession } from "../../lib/session";
-
-export async function handleLogout() {
-  await deleteSession();
-  redirect("/auth/sign-in");
-}
